a40f6b62368333b191359daf24fd7aefc2173ddc
Maigen <maigen.thomas@gmail.com>
2014-10-07 14:25:19 -0700

    header index calls for post content rather than hardcoded text

--- partials/header-index.php
+++ partials/header-index.php
@@ -1,13 +1,21 @@
       <div class="row collapse" id="banner-image">
-        <div class="banner-row"><h1>Call. To. Action.</h1></div>
-        <div class="banner-row"><h3>What do we want our patients to know?</h3></div>
+        <div class="banner-row"><h1><?php
+        $post_id = 18;
+        $queried_post = get_post($post_id);
+        echo $queried_post->post_content;
+        ?></h1></div>
+        <div class="banner-row"><h3><?php
+        $post_id = 15;
+        $queried_post = get_post($post_id);
+        echo $queried_post->post_content;
+        ?></h3></div>
       </div>
       <div class="small-12 columns" id="video-button-row">
         <div class="small-6 columns" id="video">
-          <iframe src="//player.vimeo.com/video/48914510?title=0&amp;byline=0&amp;portrait=0&amp;color=e6c973" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          <iframe src="//player.vimeo.com/video/48914510?title=0&amp;byline=0&amp;portrait=0&amp;color=e6c973" width="400" height="200" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </div>
         <div class="small-6 columns" id="things">
-          <h4>stuff goes here pls</h4>
+          <a href="/login"><div class="button-patient-login">Current Patient? Login here</div></a>
+          <a href="/new"><div class="button-new-patient">Schedule a FREE consultation</div></a>
         </div>
       </div>
-      <div class="header-row"></div>
