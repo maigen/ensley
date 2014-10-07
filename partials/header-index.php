3260e50d8e263b11c28baa39fd7fdaad15ccad44
Maigen <maigen.thomas@gmail.com>
2014-10-06 21:23:52 -0700

    video box styles updated with temp video and div. needs fixing

--- partials/header-index.php
+++ partials/header-index.php
@@ -2,5 +2,12 @@
         <div class="banner-row"><h1>Call. To. Action.</h1></div>
         <div class="banner-row"><h3>What do we want our patients to know?</h3></div>
       </div>
-      <div class="video-button-row"></div>
+      <div class="small-12 columns" id="video-button-row">
+        <div class="small-6 columns" id="video">
+          <iframe src="//player.vimeo.com/video/48914510?title=0&amp;byline=0&amp;portrait=0&amp;color=e6c973" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </div>
+        <div class="small-6 columns" id="things">
+          <h4>stuff goes here pls</h4>
+        </div>
+      </div>
       <div class="header-row"></div>
