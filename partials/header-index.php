7dc4aa70548a5e0c5daac3030c732a72eed77823
Maigen <maigen.thomas@gmail.com>
2014-10-06 20:54:09 -0700

    partialed out the banner image/call to action/video bar

---
+++ partials/header-index.php
@@ -0,0 +1,6 @@
+      <div class="row collapse" id="banner-image">
+        <div class="banner-row"><h1>Call. To. Action.</h1></div>
+        <div class="banner-row"><h3>What do we want our patients to know?</h3></div>
+      </div>
+      <div class="video-button-row"></div>
+      <div class="header-row"></div>
